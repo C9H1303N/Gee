102cee957a77d4e3ed2310b3113d3d41704f1a4f
yaozijian <yaozijian@qianxin.com>
2021-09-10 15:14:31 +0800

    group control

--- Gee/gee.go
+++ Gee/gee.go
@@ -1,29 +1,41 @@
 package Gee
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type HandlerFunc func(*Context)
 
+type RouterGroup struct {
+	prefix      string
+	middlewares []HandlerFunc // support middleware
+	engine      *Engine       // all group share one Engine instance
+}
+
 type Engine struct {
+	*RouterGroup
+	groups []*RouterGroup
 	router *router
 }
 
 func New() *Engine {
-	return &Engine{
-		router: newRouter(),
-	}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
-func (engine *Engine) addRoute(method string, pattern string, handler HandlerFunc) {
-	engine.router.addRoute(method, pattern, handler)
+func (group *RouterGroup) addRoute(method string, pattern string, handler HandlerFunc) {
+	pattern = group.prefix + pattern
+	group.engine.router.addRoute(method, pattern, handler)
 }
 
-func (engine *Engine) GET(pattern string, handler HandlerFunc) {
-	engine.addRoute("GET", pattern, handler)
+func (group *RouterGroup) GET(pattern string, handler HandlerFunc) {
+	group.addRoute("GET", pattern, handler)
 }
 
-func (engine *Engine) POST(pattern string, handler HandlerFunc) {
-	engine.addRoute("POST", pattern, handler)
+func (group *RouterGroup) POST(pattern string, handler HandlerFunc) {
+	group.addRoute("POST", pattern, handler)
 }
 
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
@@ -33,5 +45,14 @@ func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 func (engine *Engine) Run(addr string) error {
 	return http.ListenAndServe(addr, engine)
+}
 
+func (group *RouterGroup) Group(prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		engine: engine,
+	}
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
 }
--- main.go
+++ main.go
@@ -7,22 +7,31 @@ import (
 
 func main() {
 	r := Gee.New()
-	r.GET("/", func(c *Gee.Context) {
+	r.GET("/index", func(c *Gee.Context) {
+		c.HTML(http.StatusOK, "<h1>Index Page</h1>")
+	})
+	v1 := r.Group("/v1")
+
+	v1.GET("/", func(c *Gee.Context) {
 		c.HTML(http.StatusOK, "<h1>Hello Gee</h1>")
 	})
 
-	r.GET("/hello", func(c *Gee.Context) {
+	v1.GET("/hello", func(c *Gee.Context) {
 		// expect /hello?name=Geektutu
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.GET("/hello/:name/seeyou", func(c *Gee.Context) {
+	v2 := r.Group("/v2")
+
+	v2.GET("/hello/:name", func(c *Gee.Context) {
 		// expect /hello/Geektutu
 		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
 	})
-
-	r.GET("/assets/*filepath", func(c *Gee.Context) {
-		c.JSON(http.StatusOK, Gee.H{"filepath": c.Param("filepath")})
+	v2.POST("/login", func(c *Gee.Context) {
+		c.JSON(http.StatusOK, Gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
